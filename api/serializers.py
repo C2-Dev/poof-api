5a31a1f24f65bf552749ff7efad6cf3ed20bb52e
Chris <8351123+z3ke1r@users.noreply.github.com>
2019-11-06 09:53:53 -0700

    testing nested model serializer

--- api/serializers.py
+++ api/serializers.py
@@ -22,6 +22,7 @@ class FartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fart
         fields = ['user', 'score', 'type', 'smell_scale', 'noise_scale', 'city', 'date_farted']
+        depth = 1
 
 
 class TypeSerializer(serializers.HyperlinkedModelSerializer):
